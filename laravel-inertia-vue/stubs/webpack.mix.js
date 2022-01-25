19c71906cf6492fc5415663e2469fd3f61933024
Lucas Saliés Brum <lucas@archlinux.com.br>
2022-01-25 15:05:16 -0400

    Update automático

--- laravel-inertia-vue/stubs/webpack.mix.js
+++ laravel-inertia-vue/stubs/webpack.mix.js
@@ -2,7 +2,7 @@ const mix = require('laravel-mix')
 
 mix.js('resources/js/app.js', 'public/js')
     .vue()
-    .sass('resources/scss/app.scss', 'public/css')
+    .sass('resources/sass/app.scss', 'public/css')
     .sourceMaps()
     .webpackConfig(require('./webpack.config'))
     .disableNotifications()
